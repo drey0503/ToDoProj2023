e2836adf19b029f02c59efc7e6157693ad1f7fdb
Dominic Campbell <dcamp11381@gmail.com>
2023-05-22 16:43:38 -0400

    Proj

--- app/src/main/java/com/example/todoproj2023/LogInActivity.java
+++ app/src/main/java/com/example/todoproj2023/LogInActivity.java
@@ -1,4 +1,17 @@
 package com.example.todoproj2023;
 
 public class LogInActivity {
+    private String username;
+    private String password;
+
+    public Boolean checkLogin(String username, String password) {
+        if ()
+        return null;
+    }
+    private void createAccount() {
+
+    }
+    private void forgotPassword() {
+
+    }
 }
